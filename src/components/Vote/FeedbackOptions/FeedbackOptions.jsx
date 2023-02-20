0db859ed05e76f0c0636b243aa828d7db499cdfe
Anka <103377387+ann153476@users.noreply.github.com>
2023-02-20 20:37:45 +0200

    nnew

--- src/components/Vote/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Vote/FeedbackOptions/FeedbackOptions.jsx
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({leaveVote}) => {
-    return (
-        <section>
-            <h4>Leave your vote</h4>
-            <button onClick={()=> leaveVote("good")}>good</button>
-            <button onClick={()=> leaveVote("neutral")}>neutral</button>
-            <button onClick={()=> leaveVote("bad")}>bad</button>
-        </section>
-    )
-}
+const FeedbackOptions = ({ leaveVote }) => {
+  return (
+    <section>
+      <h4>Leave your vote</h4>
+      <button onClick={() => leaveVote('good')}>good</button>
+      <button onClick={() => leaveVote('neutral')}>neutral</button>
+      <button onClick={() => leaveVote('bad')}>bad</button>
+    </section>
+  );
+};
 
 FeedbackOptions.propTypes = {
-    onLeaveFeedback: PropTypes.func.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
--- src/components/Vote/vote.jsx
+++ src/components/Vote/vote.jsx
@@ -1,70 +1,76 @@
-import { Component } from "react";
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
-import Statistics from "./Statistics/Statistics";
-import Notification from "./Notification/Notification"
+import { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
 
-import s from "./vote.module.css";
+import s from './vote.module.css';
 
-const message ="There is no feedback";
+const message = 'There is no feedback';
 
 class Vote extends Component {
-    state = {
-        good : 0,
-        neutral : 0,
-        bad : 0
-    }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-    countTotalFeedback(){
-        const {good,neutral,bad}= this.state;
-        const total = good + neutral + bad;
-        return total;
-    }
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  }
 
-    countPositiveFeedbackPercentage(name) {
-        const value = this.state[name]; 
-        const total = this.countTotalFeedback();
-        if(!total) {return 0;}
-        const result = Math.round((value / total) * 100);
-        return result;
+  countPositiveFeedbackPercentage(name) {
+    const value = this.state[name];
+    const total = this.countTotalFeedback();
+    if (!total) {
+      return 0;
     }
+    const result = Math.round((value / total) * 100);
+    return result;
+  }
 
-    calcNumber(name) {
-        const total = this.countTotalFeedback();
-        if(!total) {return 0;}
-        const value = this.state[name]; 
-        const result = value;
-        return result;
+  calcNumber(name) {
+    const total = this.countTotalFeedback();
+    if (!total) {
+      return 0;
     }
+    const value = this.state[name];
+    const result = value;
+    return result;
+  }
 
-    //ця функція має бути стрілочною інакше втратимо thise
-    leaveVote = (propName) => {
-        this.setState(prevState => {
-            return {[propName]:prevState[propName]+1}
-        })
-    }
-    
-    render(){
-        const total = this.countTotalFeedback();
+  //ця функція має бути стрілочною інакше втратимо thise
+  leaveVote = propName => {
+    this.setState(prevState => {
+      return { [propName]: prevState[propName] + 1 };
+    });
+  };
 
-        const positive = this.countPositiveFeedbackPercentage("good");
-        const good = this.calcNumber("good")
-        const neutral = this.calcNumber("neutral");
-        const bad = this.calcNumber("bad");
-        return(
-            <div className={s.box}>
-                <FeedbackOptions leaveVote={this.leaveVote}/>
-                {this.countTotalFeedback()?
-                <Statistics
-                total={total} positive={positive} good={good} neutral={neutral} bad={bad}
-                />
-                :
-                <Notification message={message}/>
-                }
-                
-            </div>
-            
-        )
-    }
+  render() {
+    const total = this.countTotalFeedback();
+
+    const positive = this.countPositiveFeedbackPercentage('good');
+    const good = this.calcNumber('good');
+    const neutral = this.calcNumber('neutral');
+    const bad = this.calcNumber('bad');
+    return (
+      <div className={s.box}>
+        <FeedbackOptions leaveVote={this.leaveVote} />
+        {this.countTotalFeedback() ? (
+          <Statistics
+            total={total}
+            positive={positive}
+            good={good}
+            neutral={neutral}
+            bad={bad}
+          />
+        ) : (
+          <Notification message={message} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default Vote;
